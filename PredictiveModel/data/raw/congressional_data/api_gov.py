dd2cf4de2aa17d8a60557d2c2b76ee23f24a2d83
bryanlear <117312667+bryanlear@users.noreply.github.com>
2024-08-06 17:02:59 -0400

    selects relevant congressional bills

--- PredictiveModel/data/raw/congressional_data/api_gov.py
+++ PredictiveModel/data/raw/congressional_data/api_gov.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 import requests
-from flask import Flask, request, jsonify
+import json
+
+
 
 # Fetch congressional data uaing personal API
 api_key = 'fSJL6NcHBIKc2n8oHF1hCOYamxxmchsLWLySG7ON'
@@ -18,4 +20,28 @@ congress_data = response.json()
 # Process congressional data
 # Extract relevant information, such as bill titles, summaries, and dates
 bills = pd.DataFrame(congress_data['bills'])
-print(bills)
+
+
+#Extract bills relevant to markets 
+market_words = ['unemployment', "interest rate", "yield", "bond", "jobs", "war", 'stock', 'economy', 'trade', 'investment', 'sanction', 'geopolitical', 'immigration', 'climate', 'education']
+
+bills_of_interest = []
+
+def keywords_search():
+    for index, row in bills.iterrows(): #iterate over each row
+        title = row['title'].lower() #lowercase titles
+        if any(word in title for word in market_words):
+            bills_of_interest.append(row)
+
+
+keywords_search()
+
+
+bills_of_interest_pd = pd.DataFrame(bills_of_interest)
+
+
+print('The bills of interest are:')
+print(bills_of_interest_pd)
+
+
+
